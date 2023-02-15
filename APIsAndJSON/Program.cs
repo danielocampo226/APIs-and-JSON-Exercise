f5ced9b37cece9a614762dd85fa6d4d14348647e
danielocampo226 <sierradanny562@gmail.com>
2023-02-15 04:23:45 -0500

    APIs and JSON

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+            }
         }
     }
 }
